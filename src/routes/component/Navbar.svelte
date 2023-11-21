fd7627b9cf5ba4f2439130bd86b60403b882226f
Siravij Praevisavakij <siravijbb@gmail.com>
2023-11-21 22:27:47 +0700

    small change:accessbility

--- src/routes/component/Navbar.svelte
+++ src/routes/component/Navbar.svelte
@@ -12,6 +12,7 @@
 	import { ExclamationCircleOutline } from 'flowbite-svelte-icons';
 	let homeModal = false;
 	let aboutModal = false;
+	let contactModal = false;
 </script>
 
 <Navbar class="bg-gradient-to-b from-[#c7722e] to-[#f4a443]">
@@ -21,16 +22,16 @@
 		>
 	</NavBrand>
 	<div class="flex md:order-2">
-		<Button size="sm" class="text-gray-800 dark:text-slate-100 invisible md:visible" href="#sendwish">ส่งคำอวยพร</Button>
+		<Button size="sm" class="text-slate-100 invisible md:visible" href="#sendwish">ส่งคำอวยพร</Button>
 		<NavHamburger />
 	</div>
 	<NavUl class="order-1">
 		<NavLi href="#wish" active={true}><p class="text-white">อ่านคำอวยพร</p></NavLi>
-		<NavLi on:click={() => (homeModal = true)} class="cursor-pointer"><p class="text-white">หน้าหลัก Polygang</p></NavLi>
-		<NavLi on:click={() => (aboutModal = true)} class="cursor-pointer"
+		<NavLi active={true} on:click={() => (homeModal = true)} class="cursor-pointer hover:underline"><p class="text-white">หน้าหลัก Polygang</p></NavLi>
+		<NavLi active={true} on:click={() => (aboutModal = true)} class="cursor-pointer hover:underline"
 			><p class="text-white">เกี่ยวกับPolygang (ยังไม่เสร็จ)</p></NavLi
 		>
-		<NavLi href="https://twitter.com/SierraB_Siravij"><p class="text-white">Contact</p></NavLi>
+		<NavLi active={true}  on:click={() => (contactModal = true)} class="cursor-pointer hover:underline"><p class="text-white">Contact</p></NavLi>
 	</NavUl>
 </Navbar>
 
@@ -38,7 +39,7 @@
 	<div class="text-center">
 		<ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
 		<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-			คุณกำลังเปลี่ยนหน้าไปยังหน้าหลัก (https://polygang.fan)
+			คุณกำลังเปลี่ยนหน้าไปยังหน้าหลักpolygang (https://polygang.fan)
 		</h3>
 		<Button color="red" class="mr-2" href="https://polygang.fan">ออกจากหน้านี้</Button>
 		<Button color="alternative">ยังคงอยู่หน้านี้</Button>
@@ -48,7 +49,17 @@
 	<div class="text-center">
 		<ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
 		<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-			คุณกำลังเปลี่ยนหน้าไปยังหน้าหลัก (https://polygang.fan/about)
+			คุณกำลังเปลี่ยนหน้าไปยังข้อมูลpolygang (https://polygang.fan/about)
+		</h3>
+		<Button color="red" class="mr-2">ออกจากหน้านี้</Button>
+		<Button color="alternative">ยังคงอยู่หน้านี้</Button>
+	</div>
+</Modal>
+<Modal title="คำเตือน" bind:open={contactModal} autoclose>
+	<div class="text-center">
+		<ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
+		<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+			คุณกำลังเปลี่ยนหน้าไปยังtwitter.com (https://twitter.com/SierraB_Siravij)
 		</h3>
 		<Button color="red" class="mr-2">ออกจากหน้านี้</Button>
 		<Button color="alternative">ยังคงอยู่หน้านี้</Button>

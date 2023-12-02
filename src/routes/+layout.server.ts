902dc944a3533738e222a0df94e8816fea24e3b1
Siravij Praevisavakij <siravijbb@gmail.com>
2023-12-03 01:55:46 +0700

    error? add:prep_for disable

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -38,3 +38,43 @@ export const load: PageServerLoad = async function () {
 		birthdayWishes: { birthdayWishes: data } ///
 	};
 };
+// import { bwish } from '$db/tutorials';
+// import type { PageServerLoad } from './$types';
+// import { start_mongo, close_mongo } from '$db/mongo';
+
+// export const load: PageServerLoad = async function () {
+// 	try {
+// 		await start_mongo().then(() => {
+// 			console.log('Mongo started');
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		return {
+// 			dataFailed: true,
+// 			tutorials: undefined ///
+// 		};
+// 	}
+
+// 	const data = bwish
+// 		.find(
+// 			{},
+// 			{
+// 				projection: {
+// 					_id: 0
+// 				}
+// 			}
+// 		)
+// 		.sort({ _id: -1 })
+// 		.toArray();
+
+// 	console.log(data);
+// 	setTimeout(() => {
+// 		close_mongo().then(() => {
+// 			console.log('Mongo Closed');
+// 		});
+// 	}, 5000);
+// 	console.log('Code Closed');
+// 	return {
+// 		birthdayWishes: { birthdayWishes: data } ///
+// 	};
+// };
